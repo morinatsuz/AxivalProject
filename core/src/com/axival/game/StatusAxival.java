ff7f08297c04720e1159219cd05a8fb1cd4d782a
Sattaya Singkul <59070173@it.kmitl.ac.th>
2017-11-21 21:36:14 +0700

    fix player demo status

--- core/src/com/axival/game/StatusAxival.java
+++ core/src/com/axival/game/StatusAxival.java
@@ -12,10 +12,10 @@ public class StatusAxival {
         statusPhase = new int[13];
 
         statusPhase[0] = 0; //Amount turn
-        statusPhase[1] = 1; //character class
-        statusPhase[2] = 2; //character class
-        statusPhase[3] = 3; //character class
-        statusPhase[4] = 4; //character class
+        statusPhase[1] = 4; //character class
+        statusPhase[2] = 1; //character class
+        statusPhase[3] = 2; //character class
+        statusPhase[4] = 3; //character class
         statusPhase[5] = 0; //who's in turn
         statusPhase[6] = 0; //turn 0=draw, 1,3=action, 2=travel, 4=end
         statusPhase[7] = 0; //action start player default=0
@@ -37,17 +37,37 @@ public class StatusAxival {
         statusPhase[12] = 0; //Travel phase to row default= -1
         */
 
+        //DT status
+        statusPlayer[0][0] = 35;
+        statusPlayer[0][1] = 3;
+        statusPlayer[0][2] = 1;
+        statusPlayer[0][3] = 4;
+        statusPlayer[0][4] = 3;
+
+        //Wizard status
+        statusPlayer[1][0] = 30;
+        statusPlayer[1][1] = 3;
+        statusPlayer[1][2] = 3;
+        statusPlayer[1][3] = 6;
+        statusPlayer[1][4] = 1;
+
+        //Priest status
+        statusPlayer[2][0] = 25;
+        statusPlayer[2][1] = 3;
+        statusPlayer[2][2] = 2;
+        statusPlayer[2][3] = 3;
+        statusPlayer[2][4] = 2;
     }
 
-    public void setApInPhase(){
+    public void setApInPhase(int heroIndex){
         if (statusPhase[0]==0){
-            statusPlayer[0][1] = 3;
+            statusPlayer[heroIndex][1] = 3;
         }
         else if (statusPhase[0]<10){
-            statusPlayer[0][1] += 1;
+            statusPlayer[heroIndex][1] += 1;
         }
         else {
-            statusPlayer[0][1] = 10;
+            statusPlayer[heroIndex][1] = 10;
         }
     }
 
