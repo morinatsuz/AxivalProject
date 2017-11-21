c5259473b59afa1570a40c9f15a91e906d6d0304
ต้นกุ้งหล่อมาก <justminemind@gmail.com>
2017-11-22 03:14:16 +0700

    Auto stash before merge of "master" and "origin/master"

--- core/src/com/axival/game/screen/LoadingComponent.java
+++ core/src/com/axival/game/screen/LoadingComponent.java
@@ -80,7 +80,11 @@ public class LoadingComponent implements Screen {
         //progress + (get-pro)*lerp
         progress = MathUtils.lerp(progress, cardPlay.assetManager.getProgress(), .1f);
         if(cardPlay.assetManager.update() && progress >= cardPlay.assetManager.getProgress() - .01f){
+<<<<<<< Updated upstream
             //cardPlay.setScreen(new ScreenPlay(cardPlay, new Client()));
+=======
+          //  cardPlay.setScreen(new ScreenPlay(cardPlay, new Client()));
+>>>>>>> Stashed changes
             cardPlay.soundManager.playBgm(0);
             cardPlay.fadeScreenStage.act(delta);
         }
